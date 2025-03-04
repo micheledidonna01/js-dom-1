b4b54112490fdbb80d0d3dd0c6f262ad38a96c1c
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-04 18:34:01 +0100

    add: comments to bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -8,13 +8,29 @@ console.log(btn.innerText);
 
 //aggiungo un evento al bottone nel momento in cui viene cliccato
 btn.addEventListener('click', function(){
+
+    //se il testo del bottone è uguale ad Accendi
     if(btn.innerText === 'Accendi'){
+
+        //il testo del bottone è uguale a Spegni
         btn.innerText = 'Spegni';
+
+        //cambio la sorgente dell'immagine
         img.src = 'img/yellow_lamp.png';
+
+        //cambio l'alt dell'immagine
         img.alt = 'Lampadina accesa';
+
+    //altrimenti se il testo del bottone è uguale a Spegni
     }else if(btn.innerText === 'Spegni'){
+
+        //il testo del bottone è uguale a Spegni
         btn.innerText = 'Accendi';
+
+        //cambio la sorgente dell'immagine
         img.src = 'img/white_lamp.png';
+
+        //cambio l'alt dell'immagine
         img.alt = 'Lampadina spenta';
     
     }
